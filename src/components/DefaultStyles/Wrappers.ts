49461c7c4910822c76f98aadf1d5617f1a8e0078
Dandara Estrela <dandara_estrela@hotmail.com>
2023-06-03 11:53:45 -0300

    styles: adding global styles and wrapper

---
+++ src/components/DefaultStyles/Wrappers.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const MainWrapper = styled.div`
+  display: flex;
+  padding: 32px 10rem;
+`;
---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+  
+}
+
+body,
+input,
+textarea,
+button {
+  font-weight: 400;
+  font-size: 1rem;
+}
+
+input, button{
+   :focus,
+  :focus-within {
+    outline-style: solid;
+    outline-color: ${({ theme: { colors } }) => colors.yellowDark} !important;
+  }
+}
+`;
