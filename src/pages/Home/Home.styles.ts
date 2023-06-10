974c70c800cd5392b7214ddeb64e8086bb37f60f
Dandara Estrela <dandara_estrela@hotmail.com>
2023-06-10 11:34:12 -0300

    feat: adding beverages list

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -4,3 +4,17 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+export const BeveragesListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  row-gap: 40px;
+  column-gap: 32px;
+  padding: 32px 10rem;
+
+  @media (max-width: 800px) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+`;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,27 @@
+import { BeverageSelector } from "@/components/BeverageSelector/BeverageSelector";
 import { Banner } from "./Banner/Banner";
+
 import * as S from "./Home.styles";
+
+import { beverages } from "./Home.utils";
 
 export function Home() {
   return (
     <S.Wrapper>
       <Banner />
+      <S.BeveragesListWrapper>
+        {beverages.map(({ id, image, tags, name, description, price }) => (
+          <BeverageSelector
+            key={id}
+            id={id}
+            image={image}
+            tags={tags}
+            name={name}
+            description={description}
+            price={price}
+          />
+        ))}
+      </S.BeveragesListWrapper>
     </S.Wrapper>
   );
 }
