d2404f4f161b3a0b1c3e33dc7ebae09f8cb1e5ef
Dandara Estrela <dandara_estrela@hotmail.com>
2023-06-10 11:33:53 -0300

    feat: adding coffee mocked types

---
+++ src/assets/coffeeTypes/index.ts
@@ -0,0 +1,31 @@
+import Americano from "./Americano.png";
+import Arabe from "./Arabe.png";
+import CafeComLeite from "./Cafe-com-leite.png";
+import CafeGelado from "./Cafe-gelado.png";
+import Capuccino from "./Capuccino.png";
+import ChocolateQuente from "./Chocolate-quente.png";
+import Cubano from "./Cubano.png";
+import ExpressoCremoso from "./Expresso-cremoso.png";
+import Expresso from "./Expresso.png";
+import Havaiano from "./Havaiano.png";
+import Irlandes from "./Irlandes.png";
+import Latte from "./Latte.png";
+import Macchiato from "./Macchiato.png";
+import Mochaccino from "./Mochaccino.png";
+
+export {
+  Americano,
+  Arabe,
+  CafeComLeite,
+  CafeGelado,
+  Capuccino,
+  ChocolateQuente,
+  Cubano,
+  Expresso,
+  ExpressoCremoso,
+  Havaiano,
+  Irlandes,
+  Latte,
+  Macchiato,
+  Mochaccino,
+};
